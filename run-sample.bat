dbd4ef7e5488b0e17fc2dd4e6fe82d1693381aed
Lars Wilhelmsen <lars@sral.org>
2012-04-26 16:49:45 +0200

    Improved the run-samples.bat script to fix race condition between processes

--- run-sample.bat
+++ run-sample.bat
@@ -1,4 +1,7 @@
+rem ping commands to emulate sleep 500 (ms).
 @echo Running sample
 @start DiscoveryProxy.Console\bin\Debug\DiscoveryProxy.Console.exe
+@ping 123.45.67.89 -n 1 -w 500 > nul
 @start Samples\DiscoverableService\bin\Debug\DiscoverableService.exe
+@ping 123.45.67.89 -n 1 -w 500 > nul
 @start Samples\ClientDiscoveringServiceViaProxy\bin\Debug\ClientDiscoveringServiceViaProxy.exe
