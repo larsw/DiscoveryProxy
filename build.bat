61dd4ae210717de9e0f90328f743426735de400d
Lars Wilhelmsen <lars@sral.org>
2012-04-26 16:45:45 +0200

    Added bat files for building and running samples

---
+++ build.bat
@@ -0,0 +1,2 @@
+@call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+@msbuild /m DiscoveryProxy.sln
---
+++ run-sample.bat
@@ -0,0 +1,4 @@
+@echo Running sample
+@start DiscoveryProxy.Console\bin\Debug\DiscoveryProxy.Console.exe
+@start Samples\DiscoverableService\bin\Debug\DiscoverableService.exe
+@start Samples\ClientDiscoveringServiceViaProxy\bin\Debug\ClientDiscoveringServiceViaProxy.exe
